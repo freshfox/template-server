ec5bf67cdca0b0db99da8874945575d21c585825
Dominic Bartl <dominic.bartl.at@gmail.com>
2025-01-16 16:40:04 +0100

    ui: increase height of options window

--- src/start.ts
+++ src/start.ts
@@ -5,7 +5,12 @@ const app = createTemplateServer({
         options.test = ['1', '2', '3']
         options.locale = ['1', '2', '3']
         options.test = ['1', '2', '3']
-        options.test = ['1', '2', '3'];
+        options.test2 = ['1', '2', '3'];
+        options.test3 = ['1', '2', '3'];
+        options.test4 = ['1', '2', '3'];
+        options.test5 = ['1', '2', '3'];
+        options.test6 = ['1', '2', '3'];
+        options.test7 = ['1', '2', '3'];
         return `<html lang=""><body><div>Current query parameter</div><pre>${JSON.stringify(query, null, 2)}</pre></body></html>`
     }
 });
